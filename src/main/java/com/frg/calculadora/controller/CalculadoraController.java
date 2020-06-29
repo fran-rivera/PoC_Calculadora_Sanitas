f5a5176629e971e4142bafd161c8d4d857579922
frg86 <frgar86@gmail.com>
2020-06-30 01:33:37 +0200

    API traceo resultado de la operación;

--- src/main/java/com/frg/calculadora/controller/CalculadoraController.java
+++ src/main/java/com/frg/calculadora/controller/CalculadoraController.java
@@ -2,6 +2,7 @@ package com.frg.calculadora.controller;
 
 import com.frg.calculadora.service.CalculadoraService;
 import com.frg.calculadora.utils.Operador;
+import io.corp.calculator.TracerImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,6 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
  * Controller del API Rest
  */
 public class CalculadoraController {
+
     private CalculadoraService calculadoraService;
 
     @Autowired
@@ -20,9 +22,16 @@ public class CalculadoraController {
         this.calculadoraService = calculadoraService;
     }
 
+    private TracerImpl tracer = new TracerImpl();
+
     @GetMapping("/calculate")
     public double calcular(double op1, double op2, Operador operador) {
 
-        return calculadoraService.calcular(op1,op2,operador);
+        double result = 0;
+
+        result = calculadoraService.calcular(op1,op2,operador);
+        tracer.trace(result);
+
+        return result;
     }
 }
